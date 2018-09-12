71f12681734e5941fd9c55f314294ad1176cea85
Dae Hoon Kim <dhkim09@cafe24corp.com>
2018-09-12 11:47:05 +0900

    successfully install acacha/adminlte

--- routes/api.php
+++ routes/api.php
@@ -17,9 +17,9 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/date-time/month', 'DateTimeController@getMonth');
+Route::group(['prefix' => 'v1','middleware' => 'auth:api'], function () {
+    //    Route::resource('task', 'TasksController');
 
-Route::get('/trello', 'TrelloController@getBoardCard');
-Route::post('/trello/filter', 'TrelloController@getBoardCardFilteredByIds');
-Route::get('/trello/member', 'TrelloController@getMemberId');
-Route::get('/trello/custom-field/{customFieldName}/id', 'TrelloController@getCustomFieldId');
+    //Please do not remove this if you want adminlte:route and adminlte:link commands to works correctly.
+    #adminlte_api_routes
+});
--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,14 @@
 */
 
 Route::get('/', function () {
-    return view('dashboard');
+    return view('welcome');
 });
 
+Route::group(['middleware' => 'auth'], function () {
+    //    Route::get('/link1', function ()    {
+//        // Uses Auth Middleware
+//    });
+
+    //Please do not remove this if you want adminlte:route and adminlte:link commands to works correctly.
+    #adminlte_routes
+});
